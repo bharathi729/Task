a3df2de7facdeafbe28c4d85ca1a618d20d413c3
Bharathi Koppolu <bharathikoppolu73@gmail.com>
2023-06-13 15:52:34 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,11 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
 
 export default function App() {
+  const [originalSize, setOriginalSize] = useState(0);
+  const [compressedSize, setCompressedSize] = useState(0);
+
+  const pickVideo = async () => {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (status === 'granted') {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Videos,
+      });
+
+      if (!result.cancelled) {
+        const videoUri = result.uri;
+        const videoInfo = await FileSystem.getInfoAsync(videoUri);
+
+        setOriginalSize(videoInfo.size);
+        await compressVideo(videoUri);
+      }
+    } else {
+      console.log('Permission denied');
+    }
+  };
+
+  const compressVideo = async (videoUri) => {
+   
+    // Example code for video compression using a library ffmpeg:
+    // const compressedVideoUri = await ffmpeg.compressVideo(videoUri, options);
+    // const compressedVideoInfo = await FileSystem.getInfoAsync(compressedVideoUri);
+
+    // Mocking the compressed video size (80% reduction)
+    const compressedVideoSize = Math.round(originalSize * 0.2);
+
+    setCompressedSize(compressedVideoSize);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title="Pick a Video" onPress={pickVideo} />
+
+      {originalSize > 0 && (
+        <Text style={styles.text}>
+          Original Size: {(originalSize / (1024 * 1024)).toFixed(2)} MB
+        </Text>
+      )}
+
+      {compressedSize > 0 && (
+        <Text style={styles.text}>
+          Compressed Size: {(compressedSize / (1024 * 1024)).toFixed(2)} MB
+        </Text>
+      )}
     </View>
   );
 }
@@ -13,8 +61,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    marginVertical: 10,
   },
 });
